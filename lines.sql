6340f68cd880645da1b104b49d1d576bcf887ee2
zimaBlue7 <juan.getial@correounivalle.edu.co>
2022-03-16 17:23:31 -0500

    2.1

--- lines.sql
+++ lines.sql
@@ -2,10 +2,11 @@ CREATE database Probd;
 --- tabla admin 
 
 CREATE TABLE IF NOT EXISTS Admin(
-    id INTEGER UNIQUE,
+    id INTEGER ,
     name_ad VARCHAR(30) NOT NULL,
     code_asis INTEGER,
     CONSTRAINT py_ad PRIMARY KEY (id)
+    CONSTRAINT ad_id FOREIGN KEY (id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 INSERT INTO Admin (id,name_ad,code_asis) 
@@ -18,21 +19,22 @@ VALUES (12026227, 'Michael Pena',1001),
 
 --- tabla personal(Staff) 
 CREATE TABLE IF NOT EXISTS Staff(
-    id INTEGER UNIQUE,
+    id INTEGER,
     name_s VARCHAR(40) NOT NULL,
     speciality VARCHAR(20) NOT NULL,
     id_proof INTEGER,
     id_ques INTEGER,
     CONSTRAINT py_st PRIMARY KEY (id),
-    CONSTRAINT py_sq FOREIGN KEY (id_ques) REFERENCES Question(id) ON DELETE CASCADE
+    CONSTRAINT py_sq FOREIGN KEY (id_ques) REFERENCES Question(id) ON DELETE CASCADE,
+    CONSTRAINT st_id FOREIGN KEY (id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 INSERT INTO Staff (id,name_s,speciality,id_proof,id_ques)
-VALUES (20832768,'Jefferson Amado','Ing Sistemas',101,1),
-    (20098712,'John Sanabria', 'Ing Electronico',102,2),
-    (21928827,'Paola Ramirez','Magis Des.Soft',103,3),
+VALUES (20832768,'Jefferson Amado','Ing Sis',101,1),
+    (20098712,'John Sanabria', 'Ing Elec',102,2),
+    (21928827,'Paola Ramirez','Magis Des',103,3),
     (27716352,'Kyle Jhonson', 'Ing AI',104,4),
-    (21182111,'Leo Viviana Zuluaga','Ing Sistemas',105,1);
+    (21182111,'Viviana Zuluaga','Ing Sis',105,1);
 
 --- tabla curso (course)
 CREATE TABLE IF NOT EXISTS course(
@@ -87,13 +89,14 @@ VALUES (1,'Assi1','Fund de Redes',101,2134),
 
 --- tabla estudiantes (students)
 CREATE TABLE IF NOT EXISTS Students (
-    code serial UNIQUE,
+    code INTEGER,
     name_stu VARCHAR(40) NOT NULL,
     semester INTEGER NOT NULL,
     course VARCHAR(30) NOT NULL,
     id_staff INTEGER,
     code_asis INTEGER,
     CONSTRAINT py_stu PRIMARY KEY (code),
+    CONSTRAINT stu_id FOREIGN KEY (id) REFERENCES Users(id) ON DELETE CASCADE,
     CONSTRAINT py_sts FOREIGN KEY (id_staff) REFERENCES Staff(id) ON DELETE CASCADE,
     CONSTRAINT py_sa FOREIGN KEY (code_asis) REFERENCES Assistance(code) ON DELETE CASCADE
 );
@@ -107,26 +110,20 @@ VALUES (39218724,'Stiven Sphilberg',5,'Fund de Redes',20832768,1),
 
 --- tabla users
 CREATE TABLE IF NOT EXISTS Users (
-    id serial UNIQUE,
+    id INTEGER UNIQUE,
     name_u VARCHAR(40) NOT NULL,
     password VARCHAR(20) NOT NULL,
     description VARCHAR(50) NOT NULL,
     email VARCHAR(40) NOT NULL,
-    id_stu INTEGER,
-    id_staff INTEGER,
-    id_admin INTEGER,
-    CONSTRAINT py_us PRIMARY KEY (id),
-    CONSTRAINT py_user FOREIGN KEY (id_stu) REFERENCES Students(code) ON DELETE CASCADE,
-    CONSTRAINT py_ust FOREIGN KEY (id_staff) REFERENCES Staff(id) ON DELETE CASCADE,
-    CONSTRAINT py_ua FOREIGN KEY (id_admin) REFERENCES Admin(id) ON DELETE CASCADE
+    CONSTRAINT py_us PRIMARY KEY (id)
 );
 
-INSERT INTO Users (id,name_u, password, description, email, id_stu, id_staff, id_admin)
-VALUES (001,'Pedro Pascal',123456,'Admin secundario','pedro@gmail.com',39218724,20832768,12026227),
-    (),
-    (),
-    (),
-    ();
+INSERT INTO Users (id,name_u, password, description, email)
+VALUES (12026227,'Pedro Pascal','123456','Administrador','pedro@gmail.com'),
+    (2025987,'Camila Rodriguez','sam1307','Estudiante','cami@gmail.com'),
+    (12387288,'Mark Hamil','skywalker','Profesor','Trek12@hotmail.com'),
+    (2026223,'Juan David Olaya', 'Estudiante','Jd125@gmail.com'), 
+    (3219288,'Frank Castle','Administrador','punisher@hotmail.com'); 
 
 --- tabla pregunta (question)
 CREATE TABLE IF NOT EXISTS Question(
@@ -161,19 +158,30 @@ VALUES (1,'open',1),
     (5,'Closed',5);
 
 --- tabla opcion (Option)
+
+CREATE TYPE opMultiple AS (
+    A       varchar(1),
+    B       varchar(1),
+    C       varchar(1),
+    D       varchar(1);
+);
+
+CREATE TYPE trOrfa AS (
+    A       Boolean,
+    B       Boolean;
+);
+
 CREATE TABLE IF NOT EXISTS Option(
     id INTEGER UNIQUE,
     open VARCHAR(20),
-    multiple VARCHAR(1),
-    tof VARCHAR(5),
+    multiple opMultiple,
+    tof trOrfa,
     CONSTRAINT py_op PRIMARY KEY (id)
 );
 
-INSERT INTO Option(id,open) VALUES (1,'Modelo de tablas');
-INSERT INTO Option(id,multiple) VALUES (2,'A');
-INSERT INTO option(id,tof) VALUES (3, 'True');
-INSERT INTO Option(id,open) VALUES (4,'Return 1');
-INSERT INTO Option(id,multiple) VALUES (5 'D');
+INSERT INTO Option(id,open) VALUES (1,' ');
+INSERT INTO Option(id,multiple) VALUES (2,('A','B','C','D'));          /*modificar para estructura*/
+INSERT INTO option(id,tof) VALUES (3,(True,false));
 
 --- tabla respuesta (Answer)
 CREATE TABLE IF NOT EXISTS Answer(
@@ -186,11 +194,11 @@ CREATE TABLE IF NOT EXISTS Answer(
 );
 
 INSERT INTO Answer(id,id_stu, choose_op,date_op) 
-VALUES (11,39218724,'','21-Ene-2022'),
-    (22,38791089,'','28-Feb-2022'),
-    (33,30928172,'','2-Feb-2022'),
-    (44,31982782,'','19-Dic-2021'),
-    (55,32109827,'','1-Mar-2022');
+VALUES (11,39218724,'Modelo de tablas','2022-06-10'),
+    (22,38791089,'A','2022-12-01'),
+    (33,30928172,'True','2022-10-10'),
+    (44,31982782,'Return 1','2021-12-19'),
+    (55,32109827,'D','2022-03-01');
 
 --- Creacion de disparadores, procedimientos y vista.
 --- trigger que no permite que un estudiante este en el semestre 12 
